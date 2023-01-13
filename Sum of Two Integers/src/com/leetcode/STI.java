6616dc72720c440969ca9a1bb98b25dee0fb770c
AndrewFacada <andrewfacada@gmail.com>
2023-01-13 10:46:01 -0500

    Solution for Sum of Two Integers.

---
+++ Sum of Two Integers/src/com/leetcode/STI.java
@@ -0,0 +1,17 @@
+package com.leetcode;
+
+public class STI {
+
+	//Return the sum of two integers...
+	
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 3;
+		System.out.println(getSum(a,b));
+
+	}
+
+	public static int getSum(int a, int b) {
+        return a+b;
+    }
+}
