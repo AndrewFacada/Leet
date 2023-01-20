57d6b84cc9abad3c9f82c59937fde5dd5b4126f0
AndrewFacada <andrewfacada@gmail.com>
2023-01-20 12:35:09 -0500

    Solution for How Many Numbers Are Smaller Than The Current Number part 2.

--- How Many Numbers Are Smaller Than the Current Number/src/com/leetcode/HowManyNumbersAreSmallerThanTheCurrentNumber.java
+++ How Many Numbers Are Smaller Than the Current Number/src/com/leetcode/HowManyNumbersAreSmallerThanTheCurrentNumber.java
@@ -12,18 +12,29 @@ public class HowManyNumbersAreSmallerThanTheCurrentNumber {
 	
 	public static void main(String[] args) {
 		int[] a = {8,1,2,2,3};
+		
+		System.out.println(Arrays.toString(smallerNumbersThanCurrent(a)));
 	}
 
 	public static int[] smallerNumbersThanCurrent(int[] nums) {
-		int current = 0;
-		int large = Integer.MIN_VALUE;
-		int[] result = new int[nums.length + 1];
-		for(int i = 0; i < nums.length; i++) {
-			for(int j = 0; j < nums.length; j++) {
-				if(nums)
-			}
+		int[] result = new int[nums.length]; //Creates new array with same length as input array (nums[])
+		int goThroughResultArray = 0; //Keeps track of element for result array
+		
+		for(int i = 0; i < nums.length; i++) { //Iterates through nums[]
 			
+			for(int j = 0; j < nums.length; j++) { //Also iterates through nums[]
+				if(nums[j] == nums[i]) { //if nums[j] == nums[i] continue
+					continue;
+				}
+				
+				if(nums[i] > nums[j]) { //if nums[i] > nums[j] increment for result array because element is smaller
+					result[goThroughResultArray]++;
+				}
+			}
+			goThroughResultArray++; //Goes to next element in result array
 		}
+		
+		return result;
 	}
 
 }
