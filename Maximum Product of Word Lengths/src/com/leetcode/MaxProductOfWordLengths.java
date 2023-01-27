1d14f9ca367dc8cc2949203e944c43c001e6ef74
AndrewFacada <andrewfacada@gmail.com>
2023-01-27 08:13:04 -0500

    Need to come back to.

---
+++ Maximum Product of Word Lengths/src/com/leetcode/MaxProductOfWordLengths.java
@@ -0,0 +1,22 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class MaxProductOfWordLengths {
+	
+	/*Given a string array words, return the maximum value of 
+	 *length(word[i]) * length(word[j]) where the two words do 
+	 *not share common letters. If no such two words exist, return 0.*/
+
+	public static void main(String[] args) {
+		String[] a = {"abcw","baz","foo","bar","xtfn","abcdef"};
+		System.out.println(maxProduct(a));
+
+	}
+
+	public static int maxProduct(String[] words) {
+		
+	}
+}
