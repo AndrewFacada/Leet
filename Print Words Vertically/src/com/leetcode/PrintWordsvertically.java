aefd5e1418b79c67cbed03a0ee3632ebd2822337
AndrewFacada <andrewfacada@gmail.com>
2023-01-28 12:45:46 -0500

    Need to come back to.

---
+++ Print Words Vertically/src/com/leetcode/PrintWordsvertically.java
@@ -0,0 +1,30 @@
+package com.leetcode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintWordsvertically {
+
+	/*
+	 * Given a string s. Return all the words vertically in the same order in which
+	 * they appear in s.Words are returned as a list of strings, complete with
+	 * spaces when is necessary. (Trailing spaces are not allowed).Each word would
+	 * be put on only one column and that in one column there will be only one word.
+	 */
+
+	public static void main(String[] args) {
+		String a = "HOW ARE YOU";
+		System.out.println(printVertically(a));
+	}
+
+	public static List<String> printVertically(String s) {
+		String[] singleWords = s.split(" "); // Array with each element being a separate word in input String
+		List<String> list = new ArrayList<>();
+		
+		
+		
+		return list;
+	}
+
+}
